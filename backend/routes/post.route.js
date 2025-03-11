a37e588cca541d3d2f159bfe1196ac38d1ff5b49
Roshani Yadav <roshaniyadav0007@gmail.com>
2025-03-11 16:00:24 +0530

    3. Profile page posts and user info fetched

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
 
- import {
+import {
 	commentOnPost,
 	createPost,
 	deletePost,
 	getAllPosts,
- 	getFollowingPosts,
- 	getLikedPosts,
+	getFollowingPosts,
+	getLikedPosts,
 	getUserPosts,
 	likeUnlikePost,
 } from "../controllers/post.controller.js";
@@ -17,10 +17,10 @@ const router = express.Router();
 router.get("/all", protectRoute, getAllPosts);
 router.get("/following", protectRoute, getFollowingPosts);
 router.get("/likes/:id", protectRoute, getLikedPosts);
- router.get("/user/:username", protectRoute, getUserPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
- router.post("/like/:id", protectRoute, likeUnlikePost);
- router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/:id", protectRoute, deletePost);
 
 export default router;
--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -4,7 +4,7 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId }) => {
 	
 	const getPostEndpoint = () => {
 		switch (feedType) {
@@ -12,10 +12,10 @@ const Posts = ({ feedType }) => {
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
-			// case "posts":
-			// 	return `/api/posts/user/${username}`;
-			// case "likes":
-			// 	return `/api/posts/likes/${userId}`;
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+			   return `/api/posts/likes/${userId}`;
 			default:
 				return "/api/posts/all";
 		}
@@ -45,7 +45,7 @@ const Posts = ({ feedType }) => {
 	useEffect(() => {
 		refetch();
 
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 
 	return (
 		<>
